e0069ddfc0d141b2719a9a92048cfda4e9792717
fauziibnuhkm <114082942+fauziibnuhkm@users.noreply.github.com>
2023-01-02 00:10:23 +0700

    Toko Online

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+importScripts("https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB0yXooW8aDNPNJiqALswK_FQ6Qd6ddk7s",
+  authDomain: "toko-online-5948e.firebaseapp.com",
+  projectId: "toko-online-5948e",
+  storageBucket: "toko-online-5948e.appspot.com",
+  messagingSenderId: "116361663335",
+  appId: "1:116361663335:web:fa010bec03e4c8219795c2",
+  measurementId: "G-W2K1JFWCN1",
+});
+
+
+const messaging = firebase.messaging();
